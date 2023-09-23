e951469ac8a91d0df869494510467d5164b1f5a5
DonyStark <doniyor.rakhmanov@edu.sait.ca>
2023-09-23 12:41:57 -0600

    Updated

---
+++ cprg306-assignments/app/StudentInfo.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Your name: [Doniyor Rakhamnov]</p>
+      <p>Your course section: CPRG 306 A</p>
+      <Link href="https://github.com/DonyStark/cprg306-assignments">Visit My GitHub Repository</Link>
+    </div>
+  );
+}
+
+
+
---
+++ cprg306-assignments/app/week2/page.js
@@ -0,0 +1,15 @@
+
+import Link from 'next/link';
+import StudentInfo from '../StudentInfo.js';
+
+function Page() {
+  return (
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <StudentInfo />
+      <Link href="/week2/page.js">Go to Week 2</Link>
+    </div>
+  );
+}
+
+export default Page;
